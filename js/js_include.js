7ec898a3588a6f14ce4151c2d163bb23ce2f260b
Guru Sarath <gurusarath1.official@gmail.com>
2023-07-01 19:04:08 -0400

    Add files via upload

--- js/js_include.js
+++ js/js_include.js
@@ -1,43 +1,64 @@
-$(document).ready(function() {
-    console.log('Guru Sarath github io JS run success')
+// Reference: https://www.youtube.com/watch?v=woE-wZXiY-Q
+function loadHtml(id, filename) {
+    console.log(`id = ${id}, file = ${filename} `)
 
-    // Close all the cards 
-    var siblings = $(".card-title").siblings();
-    siblings.slideToggle();
+    let xhttp;
+    let element = document.getElementById(id);
+    let file = filename;
 
-    $(".first-title").click(
-        function(eventObj) {
-            //var siblings = $(eventObj.target).siblings()
-            //siblings.slideToggle();
-        }
-    );
-
-    $(".card-title").on(
-        {
-            click: function(eventObj) {
-                var siblings = $(eventObj.target).siblings();
-                siblings.slideToggle();
-            },
-            mouseenter: function(eventObj) {
-                var siblings = $(eventObj.target).css({"text-shadow": "1px 1px 10px #fff"});
-            },
-            mouseleave: function(eventObj) {
-                var siblings = $(eventObj.target).css({"text-shadow": "0px 0px 0px #fff"});
+    if(file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == 4) {
+                if(this.status == 200) {element.innerHTML = this.responseText;}
+                if(this.status == 404) {element.innerHTML = "Not Fount :(";}
             }
         }
-    );
+        xhttp.open("GET", `${file}`, true);
+        xhttp.send();
+        return;
+    }
 
-    $('.image-card-1').on(
-        {
-            mouseenter: function(eventObj) {
-                $(eventObj.target).children(".image-card-1-body").children(".image-card-1-body-title").css({opacity: 1});
-            },
-            mouseleave: function(eventObj) {
-                $(eventObj.target).children(".image-card-1-body").children(".image-card-1-body-title").css({opacity: 0.5});
-            },
-        }
-    );
+}
+
+function hide_about_cards(card) {
+    obj = $(".about-card")
+    obj.hide();
+}
+
+function hide_all_cards(card) {
+    obj = $(".card-title").siblings();
+    obj.slideUp();
+}
 
 
+$(document).ready(function() {
+    console.log('Guru Sarath github io JS run started');
+
+    $('#navbar').load("navbar.html", hide_all_cards);
+    $("#projects-ai-ml-dl").load("card_projects.html", hide_all_cards);
+    $("#papers-reimp").load("card_papers.html", hide_all_cards);
+    $("#blog-posts-list").load("card_blog.html", hide_all_cards);
+    $("#about-page").load("card_about.html", hide_about_cards);
+
+    // Delegated event
+    $(document).on("click", ".card-title",  function(eventObj) {
+        var siblings = $(eventObj.target).siblings();
+        siblings.slideToggle();
+    });
+
+    $(document).on("click", "#about-link",  function(eventObj) {
+        $(".about-card").slideDown();
+        $(".about-card").children().slideDown();
+    });
+
+    $(document).on("click", "#home-link",  function(eventObj) {
+        $(".about-card").slideUp();
+    });
+
+
+    console.log('Guru Sarath github io JS run SUCESS');
 });
+
+
 
